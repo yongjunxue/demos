626ea17ebf1b368f3bec8bdd57d7e790c3b9572c
薛永俊 <1925213936@qq.com>
2019-10-18 13:15:48 +0800

    1.

---
+++ src/test/java/com/jfreechart/demo/demo/Tests.java
@@ -0,0 +1,14 @@
+package com.jfreechart.demo.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public class DemoApplicationTests {
+
+	public void contextLoads() {
+		//1
+	}
+
+}
