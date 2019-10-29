583b0cd60245e1ee9e5c4f07233f2753a9884826
薛永俊 <1925213936@qq.com>
2019-10-29 13:54:29 +0800

    1.branch2添加

--- src/test/java/com/jfreechart/demo/demo/Tests.java
+++ src/test/java/com/jfreechart/demo/demo/Tests.java
@@ -1,16 +1,13 @@
 package com.jfreechart.demo.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-public class DemoApplicationTests {
+public class Tests {
 
 	public void contextLoads() {
 		//1
-		//2
+		//2    branch2
 		//3
+
+		//4branch2
 	}
 
 }
